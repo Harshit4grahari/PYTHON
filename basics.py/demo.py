2f2fd22ca36664fa6a42047893b9517c991b9e54
Harshit Agrahari <agrahariagrahari406@gmail.com>
2026-02-14 20:08:00 +0530

    About Basics of Python

--- basics.py/demo.py
+++ basics.py/demo.py
@@ -1,1 +1,4 @@
-print("hello")
+print("Hello") #printing command  
+print("My name is Harshit Agrahari") 
+print("21") #for printing age
+print("I am a student of Computer Science") 
---
+++ basics.py/variables.py
@@ -0,0 +1,7 @@
+#Variables
+name = "Harshit Agrahari"  #string variable for name
+age = 21
+course = "Computer Science"
+print("My name is", name) #printing name using variable
+print("I am", age, "years old") #printing age using variable    
+print("I am studying", course)  #printing course using variable
