6ccfcdb7a4f11904b54d06bb9539dbcd39468546
Harshit Agrahari <agrahariagrahari406@gmail.com>
2026-05-06 20:42:46 +0530

    Basics of Python

---
+++ Basics/Functions2.py
@@ -0,0 +1,12 @@
+def calc_sum(a, b): #Function definition with parameters a and b
+    s = a + b
+    print(s)
+    return s
+
+calc_sum(10, 20) #Calling Function with Argument 10 and 20
+
+#more lines of code
+calc_sum(5, 15)
+
+#more lines of code
+calc_sum(7, 3)
---
+++ Questions/BasicQues23.py
@@ -0,0 +1,9 @@
+#WAF to print the length of a list.(list is the parameter)
+
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+heroes = ["Superman", "Batman", "Wonder Woman", "Flash", "Green Lantern"]
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(heroes)
